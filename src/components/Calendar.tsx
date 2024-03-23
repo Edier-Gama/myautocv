2f89111e4f12ee8ef5255bcbb1bb9c7a8a129065
Edier Gama <ediersantiago7@gmail.com>
2024-03-23 00:21:44 -0500

    Margin top para responsive

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -12,17 +12,32 @@ const options = {
   clearBtnText: 'Clear',
   maxDate: new Date('2030-01-01'),
   minDate: new Date('1950-01-01'),
+  // theme: {
+  //   background: 'bg-white',
+  //   todayBtn: '',
+  //   clearBtn: '',
+  //   icons: '',
+  //   text: 'Onest Variable',
+  //   input: '',
+  //   inputIcon: '',
+  //   selected: ''
+  // },
   icons: {
     prev: () => <span>Back</span>,
     next: () => <span>Next</span>
   },
-  datepickerClassNames: 'font-sans ml-10',
+  datepickerClassNames: 'font-sans ml-10 mt-80 md:mt-5',
   language: 'en',
   disabledDates: [],
   weekDays: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
   inputNameProp: 'date',
   inputIdProp: 'date',
   inputPlaceholderProp: 'Fecha de inicio'
+  // inputDateFormatProp: {
+  //   day: 'numeric',
+  //   month: 'long',
+  //   year: 'numeric'
+  // }
 }
 
 const CalendarStartAndFinish = (): any => {
