a7d254360baafa0a0cccb39e137bab01b877c696
Edier Gama <ediersantiago7@gmail.com>
2024-04-06 23:38:26 -0500

    change to MyAutoCV for better SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,11 @@ import '@fontsource-variable/nunito'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Auto CV - Crea CV en segundos con Inteligencia Artificial',
-  description: 'Auto CV te permite crear un CV minimalista y anti ATS manualmente o de manera automática usando inteligencia artificial y tu perfil de LinkedIn.',
+  title: 'My AutoCV - Crea CV en segundos con Inteligencia Artificial',
+  description: 'My AutoCV te permite crear un CV minimalista y anti ATS manualmente o de manera automática usando inteligencia artificial y tu perfil de LinkedIn.',
   openGraph: {
-    title: 'Auto CV - Crea CV en segundos con Inteligencia Artificial',
-    description: 'Auto CV te permite crear un CV minimalista y anti ATS manualmente o de manera automática usando inteligencia artificial y tu perfil de LinkedIn.',
+    title: 'My AutoCV - Crea CV en segundos con Inteligencia Artificial',
+    description: 'My AutoCV te permite crear un CV minimalista y anti ATS manualmente o de manera automática usando inteligencia artificial y tu perfil de LinkedIn.',
     images: '/favicon.svg'
   }
 }
@@ -28,17 +28,17 @@ export default function RootLayout ({
       <link rel="canonical" href='https://myautocv.vercel.app/' />
       <meta name="viewport" content="width=device-width" />
       <meta name="theme-color" content="#0000" />
-      <meta name="keywords" content="auto cv, Auto cv, AutoCV, EdierGama, Edier Gama, auto CV, AutoCV, CV, Cv rapido, crear CV con IA, cv maker" />
+      <meta name="keywords" content="myautocv, MyAutocv, MyAutoCV, My Auto cv, my auto cv, EdierGama, Edier Gama, auto CV, AutoCV, CV, Cv rapido, crear CV con IA, cv maker" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@edier_gama" />
       <meta name="twitter:creator" content="@edier_gama" />
-      <meta name="twitter:title" content='AutoCV CEO'/>
-      <meta name="twitter:description" content='Auto CV te permite crear un CV minimalista y anti ATS manualmente o de manera automática usando inteligencia artificial y tu perfil de LinkedIn' />
+      <meta name="twitter:title" content='My AutoCV CEO'/>
+      <meta name="twitter:description" content='My AutoCV te permite crear un CV minimalista y anti ATS manualmente o de manera automática usando inteligencia artificial y tu perfil de LinkedIn' />
       <meta name="og:image" content='' />
-      <meta name="og:title" content='Auto CV - Crea CV en segundos con Inteligencia Artificial' />
-      <meta name="og:description" content='Auto CV te permite crear un CV minimalista y anti ATS manualmente o de manera automática usando inteligencia artificial y tu perfil de LinkedIn' />
+      <meta name="og:title" content='My AutoCV - Crea CV en segundos con Inteligencia Artificial' />
+      <meta name="og:description" content='My AutoCV te permite crear un CV minimalista y anti ATS manualmente o de manera automática usando inteligencia artificial y tu perfil de LinkedIn' />
       <meta name="og:url" content='https://myautocv.vercel.app' />
-      <meta name="og:site_name" content="Auto cv" />
+      <meta name="og:site_name" content="My AutoCV" />
       <meta name="og:type" content="website" />
       <meta name="og:locale" content="co_CO" />
       <meta name="robots" content="index, follow" />
