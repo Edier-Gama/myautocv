563b65aed58806492aea91170cb3df29cda7b370
Edier Gama <ediersantiago7@gmail.com>
2024-04-08 22:07:13 -0500

    change redirect to production url

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 import { type CookieOptions, createServerClient } from '@supabase/ssr'
@@ -10,9 +13,7 @@ export async function GET (request: Request) {
   if (code != null) {
     const cookieStore = cookies()
     const supabase = createServerClient(
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
       {
         cookies: {
@@ -20,21 +21,18 @@ export async function GET (request: Request) {
             return cookieStore.get(name)?.value
           },
           set (name: string, value: string, options: CookieOptions) {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
             cookieStore.set({ name, value, ...options })
           },
           remove (name: string, options: CookieOptions) {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
             cookieStore.delete({ name, ...options })
           }
         }
       }
     )
-    const { error } = await supabase.auth.exchangeCodeForSession(code)
-    if (error == null) {
-      return NextResponse.redirect(origin)
-    }
+    console.log(code)
+    await supabase.auth.exchangeCodeForSession(code)
+    return NextResponse.redirect(`${origin}${next}`)
+  } else {
+    return NextResponse.redirect(`${origin}/auth/auth-code-error`)
   }
-
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`)
 }
--- src/utils/supabase/sign-in-with-linkedin.ts
+++ src/utils/supabase/sign-in-with-linkedin.ts
@@ -1,8 +1,9 @@
+'use client'
+
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { createBrowserClient } from '@supabase/ssr'
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const NEXT_PUBLIC_SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const NEXT_PUBLIC_SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 const SignInWithLinkedin = async () => {
   const supabase = createBrowserClient(NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY)
