6f626c820588b4ac441493fa37a33bf0aa0ece5f
Edier Gama <ediersantiago7@gmail.com>
2024-04-18 22:30:28 -0500

    Trying to solve server bugs

--- src/components/HeaderServer.tsx
+++ src/components/HeaderServer.tsx
@@ -5,20 +5,11 @@ export default async function HeaaderServer () {
   const supabase = await createSupabaseServerClient()
   const { data } = await supabase.from('users').select('*')
 
-  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-  if (data) {
-    const session = await getUserSession()
-    const selectedUser = data.find((user: any) => {
-      // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-      if (!session) {
-        return null
-      }
-      const userData = user.provider_id === session.user.user_metadata.provider_id
-      return userData
-    })
-    const userAvatar = selectedUser.picture
-    return <HeaderClient avatar={userAvatar} user={selectedUser}/>
-  } else {
-    return undefined
-  }
+  const session = await getUserSession()
+  const selectedUser = data?.find((user: any) => {
+    const userData = user.provider_id === session?.user.user_metadata.provider_id
+    return userData
+  })
+  const userAvatar = selectedUser.picture
+  return <HeaderClient avatar={userAvatar} user={selectedUser}/>
 }
