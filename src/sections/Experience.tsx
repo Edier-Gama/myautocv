3bfc026aaebe8001ce695e51f1cd0a9415199dcc
Edier Gama <ediersantiago7@gmail.com>
2024-05-07 23:24:24 -0500

    Algunos reparos en la UI

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -31,14 +31,14 @@ function Experience () {
          <section className="experience-section m-3 max-w-7xl justify-center items-center mt-20" key={experience.company}>
            <section className="flex flex-wrap">
              <div className="flex flex-col">
-               <h1 className="font-sans text-2xl font-bold text-left">{experience.company}</h1>
-               <p className="font-sans text-xl max-w-xl mt-2 font-bold">{experience.charge}</p>
-               <div className="flex justify-right">
-                 <p className="font-sans text-xl text-left mt-2">{initParsed} - </p>
+               <h1 className="font-sans text-2xl font-bold text-left max-w-56">{experience.company}</h1>
+               <p className="font-sans text-xl mt-2 font-bold max-w-56">{experience.charge}</p>
+               <div className="flex justify-right max-w-64">
+                 <p className="font-sans text-xl text-left mt-2">{initParsed} </p>
                  <p className="font-sans text-xl text-left ml-2 mt-2">{finishParsed}</p>
                </div>
              </div>
-             <p className="font-sans text-xl mt-5 md:mt-1 max-w-4xl md:max-w-lg md:ml-20 text-left">{experience.jobDescription}</p>
+             <p className="font-sans text-xl max-w-96 md:max-w-lg md:ml-20 text-left">{experience.jobDescription}</p>
            </section>
          </section>
       )
