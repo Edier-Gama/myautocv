b5d86572663d3ee20ba296ce83f259f41e7fd5e5
Edier Gama <ediersantiago7@gmail.com>
2024-05-08 23:05:52 -0500

    appying fonts

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -36,15 +36,15 @@ function Experience () {
           <div className="sticky top-0">
             <span className="text-blue-600 -left-[44px] absolute rounded-full text-5xl"
             >&bull;</span>
-            <h4 className="font-semibold text-3xl text-black dark:text-white">{experience.company}</h4>
-            <h3 className="mt-3 text-xl font-bold text-black">{experience.charge}</h3>
+            <h4 className="font-semibold text-3xl text-black dark:text-white font-sans">{experience.company}</h4>
+            <h3 className="mt-3 text-xl font-bold text-black font-sans">{experience.charge}</h3>
             <div className="flex flex-wrap">
-            <time className="mt-3 p-0 m-0 text-xl text-black">{initParsed}</time>
-            <time className="mt-3 p-0 m-0 text-xl text-black ml-3"> {finishParsed}</time>
+            <time className="mt-3 p-0 m-0 text-xl text-black font-sans">{initParsed}</time>
+            <time className="mt-3 p-0 m-0 text-xl text-black ml-3 font-sans"> {finishParsed}</time>
             </div>
           </div>
         </div>
-        <div className="text-xl max-w-80 md:max-w-4xl text-black relative flex flex-col gap-2 pb-4  md:col-span-3">
+        <div className="text-xl max-w-80 md:max-w-4xl text-black relative flex flex-col gap-2 pb-4  md:col-span-3 font-sans">
           {experience.jobDescription}
         </div>
       </section>
